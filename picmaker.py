84254696126b012e9347a21fa55e3613ccb2b9fc
horazio <hfusco@stuy.edu>
2019-02-03 23:20:14 -0500

    there it is

--- picmaker.py
+++ picmaker.py
@@ -2,10 +2,21 @@ f = open("image.ppm", "w+")
 f.write("P3\n500 500\n255\n")
 
 
+ray = []
+for i in range(500):
+    ray.append(0)    
+
+ray[0] = 1
 
 for i in range(500):
+ 
+    for x in range(500):
+        if(x < 499):
+            ray[x + 1] += ray[x]
+            ray[x + 1] %= 2
+            
     for j in range(500):
-        pixel = str(i) + " " + str(j) + " 0 "
+        pixel = str(i * j % 255) + " " + str(255 * ray[j]) + " " + str(j * i % 255) + " "
         f.write(pixel) 
 
 f.close()    
